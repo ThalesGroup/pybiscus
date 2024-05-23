29b082590dfaf3baa14abd8d813f430cff29b2c4
Baptiste Morisse <bmorisse.pro@proton.me>
2024-05-23 16:02:38 +0200

    Refacto: better CLI server with jsonargparse
    Refacto: create a Server class to hold alla data, model, Fabric instance... and a launch method.
    Use of "instantiate" command in order to instantiate all subclasses given in CLI.
    Modify the config file in order to take into account the new CLI

--- pybiscus/commands/app_server.py
+++ pybiscus/commands/app_server.py
@@ -1,132 +1,27 @@
-import flwr as fl
-import torch
 from jsonargparse import ActionConfigFile, ArgumentParser
-from lightning.fabric import Fabric
-from lightning.fabric.loggers import TensorBoardLogger
-from omegaconf import OmegaConf
 from rich.traceback import install
 
 from pybiscus.console import console
-from pybiscus.flower.callbacks import (
-    better_aggregate,
-    evaluate_config,
-    fit_config,
-    get_evaluate_fn,
-    weighted_average_metrics,
-)
-from pybiscus.flower.server_fabric import ConfigServer
-from pybiscus.ml.registry import datamodule_registry, model_registry
+from pybiscus.flower.server_fabric import Server
 
 install()
 
 
-def launch_server(
-    config_server: ConfigServer,
-):
-    """Launch a Flower Server.
-
-    This is a Typer command to launch a Flower Server, using the configuration given by config.
-    Apart from the config parameter, other parameters are optional and, if given, override the associated parameter given by the parameter config.
-
-    Parameters
-    ----------
-    config:
-        path to a config file
-    num_rounds: optional
-        the number of Federated rounds
-    server_adress: optional
-        the IP adress and port of the Flower Server.
-    weights_path: optional
-        path to the weights of the model to be loaded at the beginning of the Federated Learning.
-
-    Raises
-    ------
-    ValidationError
-        the Pydantic error raised if the config is not validated.
-
-    """
-    console.log(config_server)
-    conf = dict(config_server)
-    console.log(conf)
-    conf_fabric = dict(config_server["fabric"])
-    conf_data = dict(config_server["data"].config)
-    conf_model = dict(config_server["model"].config)
-    conf_strategy = dict(config_server["strategy"].config)
-
-    logger = TensorBoardLogger(root_dir=conf["root_dir"] + conf["logger"]["subdir"])
-    fabric = Fabric(**conf_fabric, loggers=logger)
-    fabric.launch()
-    model_class = model_registry[config_server["model"].name]
-    model = model_class(**conf_model)
-    model = fabric.setup_module(model)
-    data = datamodule_registry[config_server["data"].name](**conf_data)
-    data.setup(stage="test")
-    _test_set = data.test_dataloader()
-    test_set = fabric._setup_dataloader(_test_set)
-
-    initial_parameters = None
-    if config_server["weights_path"] is not None:
-        state = fabric.load(config_server["weights_path"])["model"]
-        model.load_state_dict(state)
-
-        params = torch.nn.ParameterList(
-            [param.detach().numpy() for param in model.parameters()]
-        )
-        initial_parameters = fl.common.ndarrays_to_parameters(params)
-        console.log(f"Loaded weights from {config_server['weights_path']}")
-    else:
-        params = torch.nn.ParameterList(
-            [param.detach().numpy() for param in model.parameters()]
-        )
-        initial_parameters = fl.common.ndarrays_to_parameters(params)
-
-    strategy = fl.server.strategy.FedAvg(
-        fit_metrics_aggregation_fn=better_aggregate(
-            weighted_average_metrics, fabric=fabric, stage="fit", start_server_round=1
-        ),
-        evaluate_metrics_aggregation_fn=better_aggregate(
-            weighted_average_metrics, fabric=fabric, stage="val", start_server_round=1
-        ),
-        evaluate_fn=get_evaluate_fn(testset=test_set, model=model, fabric=fabric),
-        on_fit_config_fn=fit_config,
-        on_evaluate_config_fn=evaluate_config,
-        initial_parameters=initial_parameters,
-        **conf_strategy,
-    )
-
-    fl.server.start_server(
-        server_address=conf["server_adress"],
-        config=fl.server.ServerConfig(num_rounds=conf["num_rounds"]),
-        strategy=strategy,
-    )
-
-    if conf["save_on_train_end"]:
-        state = {"model": model}
-        fabric.save(fabric.logger.log_dir + "/checkpoint.pt", state)
-
-    with open(fabric.logger.log_dir + "/config_server_launch.yml", "w") as file:
-        OmegaConf.save(config=conf, f=file)
-    if conf["client_configs"] is not None:
-        for client_conf in conf["client_configs"]:
-            console.log(client_conf)
-            _conf = OmegaConf.load(client_conf)
-            with open(
-                fabric.logger.log_dir
-                + f"/config_client_{_conf['config_client']['cid']}_launch.yml",
-                "w",
-            ) as file:
-                OmegaConf.save(config=_conf, f=file)
-
-
 def server_cli():
     parser = ArgumentParser(description="This is it!", parser_mode="omegaconf")
-    parser.add_argument("--config_server", type=ConfigServer)
+    # parser.add_argument("--config_server", type=ConfigServer)
+    parser.add_class_arguments(Server, "server.init")
+    parser.add_method_arguments(Server, "launch", "server.launch")
 
     parser.add_argument("--config", action=ActionConfigFile)
 
     cfg = parser.parse_args()
+    console.log(cfg.as_dict())
+    cfg = parser.instantiate_classes(cfg)
     console.log(cfg)
-    launch_server(config_server=cfg["config_server"])
+    server = cfg.server.init
+    console.log(server)
+    server.launch(**cfg.server.launch.as_dict())
 
 
 if __name__ == "__main__":
--- pybiscus/flower/server_fabric.py
+++ pybiscus/flower/server_fabric.py
@@ -1,11 +1,26 @@
 from pathlib import Path
+from typing import Optional
 
+import flwr as fl
+import torch
+from lightning.fabric import Fabric
+from lightning.fabric.loggers import TensorBoardLogger
+from omegaconf import OmegaConf
 from pydantic import BaseModel, ConfigDict, Field
 
+from pybiscus.console import console
+from pybiscus.flower.callbacks import (
+    better_aggregate,
+    evaluate_config,
+    fit_config,
+    get_evaluate_fn,
+    weighted_average_metrics,
+)
 from pybiscus.flower.client_fabric import ConfigFabric
 from pybiscus.flower.strategies import ConfigFabricStrategy
 from pybiscus.ml.data.cifar10.cifar10_datamodule import ConfigData_Cifar10
 from pybiscus.ml.models.cnn.lit_cnn import ConfigModel_Cifar10
+from pybiscus.ml.registry import datamodule_registry, model_registry
 
 
 class ConfigStrategy(BaseModel):
@@ -61,3 +76,128 @@ class ConfigServer(BaseModel):
     weights_path: Path = Field(default=None)
 
     model_config = ConfigDict(extra="forbid")
+
+
+class Server:
+    def __init__(
+        self,
+        root_dir: Path,
+        # conf_logger: dict,
+        logger: TensorBoardLogger,
+        conf_strategy: ConfigStrategy,
+        conf_fabric: ConfigFabric,
+        conf_model: ConfigModel_Cifar10,
+        conf_data: ConfigData_Cifar10,
+        weights_path: Optional[Path],
+    ):
+        """Launch a Flower Server.
+
+        This is a Typer command to launch a Flower Server, using the configuration given by config.
+        Apart from the config parameter, other parameters are optional and, if given, override the associated parameter given by the parameter config.
+
+        Parameters
+        ----------
+        config:
+            path to a config file
+        num_rounds: optional
+            the number of Federated rounds
+        server_adress: optional
+            the IP adress and port of the Flower Server.
+        weights_path: optional
+            path to the weights of the model to be loaded at the beginning of the Federated Learning.
+
+        Raises
+        ------
+        ValidationError
+            the Pydantic error raised if the config is not validated.
+
+        """
+        conf_fabric = dict(conf_fabric)
+        name_data = conf_data.name
+        conf_data = dict(conf_data.config)
+        name_model = conf_model.name
+        conf_model = dict(conf_model.config)
+        # name_strategy = conf_strategy.name
+        conf_strategy = dict(conf_strategy.config)
+        # conf_logger = dict(conf_logger)
+
+        # self.logger = TensorBoardLogger(root_dir=root_dir + conf_logger["subdir"])
+        self.logger = logger
+
+        self.fabric = Fabric(**conf_fabric, loggers=self.logger)
+        self.fabric.launch()
+        model_class = model_registry[name_model]
+        model = model_class(**conf_model)
+        self.model = self.fabric.setup_module(model)
+        data = datamodule_registry[name_data](**conf_data)
+        data.setup(stage="test")
+        _test_set = data.test_dataloader()
+        self.test_set = self.fabric._setup_dataloader(_test_set)
+
+        initial_parameters = None
+        if weights_path is not None:
+            state = self.fabric.load(weights_path)["model"]
+            self.model.load_state_dict(state)
+
+            params = torch.nn.ParameterList(
+                [param.detach().numpy() for param in model.parameters()]
+            )
+            initial_parameters = fl.common.ndarrays_to_parameters(params)
+            console.log(f"Loaded weights from {weights_path}")
+        else:
+            params = torch.nn.ParameterList(
+                [param.detach().numpy() for param in model.parameters()]
+            )
+            initial_parameters = fl.common.ndarrays_to_parameters(params)
+
+        self.strategy = fl.server.strategy.FedAvg(
+            fit_metrics_aggregation_fn=better_aggregate(
+                weighted_average_metrics,
+                fabric=self.fabric,
+                stage="fit",
+                start_server_round=1,
+            ),
+            evaluate_metrics_aggregation_fn=better_aggregate(
+                weighted_average_metrics,
+                fabric=self.fabric,
+                stage="val",
+                start_server_round=1,
+            ),
+            evaluate_fn=get_evaluate_fn(
+                testset=self.test_set, model=model, fabric=self.fabric
+            ),
+            on_fit_config_fn=fit_config,
+            on_evaluate_config_fn=evaluate_config,
+            initial_parameters=initial_parameters,
+            **conf_strategy,
+        )
+
+    def launch(
+        self,
+        server_adress: str,
+        num_rounds: int,
+        client_configs: list[str],
+        save_on_train_end: bool,
+    ):
+        fl.server.start_server(
+            server_address=server_adress,
+            config=fl.server.ServerConfig(num_rounds=num_rounds),
+            strategy=self.strategy,
+        )
+
+        if save_on_train_end:
+            state = {"model": self.model}
+            self.fabric.save(self.fabric.logger.log_dir + "/checkpoint.pt", state)
+
+        # with open(self.fabric.logger.log_dir + "/config_server_launch.yml", "w") as file:
+        #     OmegaConf.save(config=conf, f=file)
+        if client_configs is not None:
+            for client_conf in client_configs:
+                console.log(client_conf)
+                _conf = OmegaConf.load(client_conf)
+                with open(
+                    self.fabric.logger.log_dir
+                    + f"/config_client_{_conf['config_client']['cid']}_launch.yml",
+                    "w",
+                ) as file:
+                    OmegaConf.save(config=_conf, f=file)
