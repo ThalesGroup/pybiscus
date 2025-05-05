5c64635d3c5fa244d5dc85401ada8a00b478dea7
Laurent Morel <laurent.morel@thalesgroup.com>
2025-05-05 17:38:35 +0200

    Metrics log web-hook fix

--- pybiscus/core/metricslogger/webhook/webhooklogger.py
+++ pybiscus/core/metricslogger/webhook/webhooklogger.py
@@ -4,14 +4,13 @@ from requests.exceptions import RequestException, Timeout
 class WebHookLogger():
 
     def __init__(self, root_dir, webhook_url):
-        self.root_dir = root_dir
+        self.log_dir = root_dir
         self.webhook_url = webhook_url
-        self.webhook_url2 = "http://localhost:9999/log_metrics"
 
     def log_metrics(self, metrics, step):
         
         try:
-            response = requests.post(self.webhook_url2, json=metrics, timeout=5)
+            response = requests.post(self.webhook_url, json=metrics, timeout=5)
             response.raise_for_status()  # raise an exception upon codes 4xx/5xx
 
             print("✅ Metrics log Webhook call success")
