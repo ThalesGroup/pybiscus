27c39d90ecd136ac4b0a96bef77114fc477fdeab
Laurent Morel <laurent.morel@thalesgroup.com>
2025-07-25 10:48:07 +0200

    tests with fabric configuration
    add commented out possible extensions

--- pybiscus/flower_config/config_hardware.py
+++ pybiscus/flower_config/config_hardware.py
@@ -15,19 +15,68 @@ class ConfigHardware(BaseModel):
     devices: optional
         either an integer (the number of devices needed); a list of integers (the id of the devices); or
         the string "auto" to let Fabric choose the best option available.
-    """
+
+        
+
+        
+    copy of the source code from installed package (provision for future enhancements):
+            
+    class Fabric:
+        Fabric accelerates your PyTorch training or inference code with minimal changes required.
+
+        - Automatic placement of models and data onto the device.
+        - Automatic support for mixed and double precision (smaller memory footprint).
+        - Seamless switching between hardware (CPU, GPU, TPU) and distributed training strategies
+        (data-parallel training, sharded training, etc.).
+        - Automated spawning of processes, no launch utilities required.
+        - Multi-node support.
+
+        Args:
+            accelerator: The hardware to run on. Possible choices are:
+                ``"cpu"``, ``"cuda"``, ``"mps"``, ``"gpu"``, ``"tpu"``, ``"auto"``.
+            strategy: Strategy for how to run across multiple devices. Possible choices are:
+                ``"dp"``, ``"ddp"``, ``"ddp_spawn"``, ``"deepspeed"``, ``"fsdp"``.
+            devices: Number of devices to train on (``int``), which GPUs to train on (``list`` or ``str``), or ``"auto"``.
+                The value applies per node.
+            num_nodes: Number of GPU nodes for distributed training.
+            precision: Double precision (``"64"``), full precision (``"32"``), half precision AMP (``"16-mixed"``),
+                or bfloat16 precision AMP (``"bf16-mixed"``).
+            plugins: One or several custom plugins
+            callbacks: A single callback or a list of callbacks. A callback can contain any arbitrary methods that
+                can be invoked through :meth:`~lightning.fabric.fabric.Fabric.call` by the user.
+            loggers: A single logger or a list of loggers. See :meth:`~lightning.fabric.fabric.Fabric.log` for more
+                information.
+
+    
+        def __init__(
+            self,
+            *,
+            accelerator: Union[str, Accelerator] = "auto",
+            strategy: Union[str, Strategy] = "auto",
+            devices: Union[List[int], str, int] = "auto",
+            num_nodes: int = 1,
+            precision: Optional[_PRECISION_INPUT] = None,
+            plugins: Optional[Union[_PLUGIN_INPUT, List[_PLUGIN_INPUT]]] = None,
+            callbacks: Optional[Union[List[Any], Any]] = None,
+            loggers: Optional[Union[Logger, List[Logger]]] = None,
+        ) -> None:
+"""
 
     PYBISCUS_CONFIG: ClassVar[str] = "hardware"
 
     class Accelerator(str, Enum):
         cpu  = "cpu"
+        # cuda = "cuda"
+        # mps  = "mps"
         gpu  = "gpu"
+        # tpu  = "tpu"
         auto = "auto"
 
     accelerator: Accelerator = "auto"
 
     # TODO: check devices
     # devices: Union[int, list[int], str] = "auto"
+    # NB: int means the requested number of devices
     devices: Union[int, str] = "auto"
 
     model_config = ConfigDict(extra="forbid")
